8b320b731b236b30ae4971c54b4977670afcb69e
clxmente <solorioeclem164@gmail.com>
2022-10-18 19:58:40 -0700

    feat: startup script populates db

--- share/wordle.sql
+++ share/wordle.sql
@@ -1,26 +1,36 @@
--- TODO: create tables
+PRAGMA foreign_keys=ON;
 BEGIN TRANSACTION;
-DROP TABLE IF EXISTS game_states, games, users;
 
-CREATE TABLE game_states (
-    gameid INTEGER,
-    guesses INTEGER(1) NOT NULL,
-    correct INTEGER(1) NOT NULL,
-    incorrect INTEGER(1) NOT NULL,
-    PRIMARY KEY(gameid)
+DROP TABLE IF EXISTS valid_words;
+DROP TABLE IF EXISTS game_states;
+DROP TABLE IF EXISTS games;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    username TEXT NOT NULL,
+    password TEXT NOT NULL,
+    PRIMARY KEY(username)
 );
 
 CREATE TABLE games (
-    gameid INTEGER,
-    secretword VARCHAR(5) NOT NULL,
-    username VARCHAR(20),
-    PRIMARY KEY(gameid)
+    gameid INTEGER NOT NULL,
+    secretword TEXT NOT NULL,
+    username TEXT NOT NULL,
+    PRIMARY KEY(gameid),
+    FOREIGN KEY(username) REFERENCES users(username)
 );
 
-CREATE TABLE users (
-    username VARCHAR(20) NOT NULL,
-    password VARCHAR(20) NOT NULL,
-    PRIMARY KEY(username)
+CREATE TABLE game_states (
+    gameid INTEGER NOT NULL,
+    guesses TINYINT NOT NULL,
+    correct TEXT NOT NULL,
+    incorrect TEXT NOT NULL,
+    FOREIGN KEY (gameid) REFERENCES games(gameid)
+);
+
+CREATE TABLE valid_words (
+    word TEXT NOT NULL,
+    PRIMARY KEY(word)
 );
 
 COMMIT;
