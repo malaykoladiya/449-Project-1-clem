a1bdd378e5084ff245e39101b9729b1b4c258b19
divyanshrao38 <divyanshrao38@gmail.com>
2022-12-01 14:09:21 -0800

    added code to retrieve leaders

--- leaderboard.py
+++ leaderboard.py
@@ -27,6 +27,7 @@ class Leaderboard:
 
 r = redis.StrictRedis()
 r.flushdb()
+playerrank = "playerrank"
 
 # ---------------------------------------------------------------------------- #
 #                                error handlers                                #
@@ -80,7 +81,7 @@ async def updateScore(data):
             pointstoadd+=currentScore
         print('gemainfo',gameinfo,pointstoadd)
         print("initial",r.keys())
-        playerrank = "playerrank"
+        
     
         r.hmset("players",{gameinfo['username']:pointstoadd})
         testdict = {}
@@ -93,3 +94,32 @@ async def updateScore(data):
         abort(500,"Internal Server Error Cant update Leaderboard,please try again by refining the input parameters")
 
     return f"leaderboard Updated sucessfully for {gameinfo['username']} with score {pointstoadd}", 201
+
+
+# ----------------------------Retrieve top 10------------------------------------
+@app.route("/leaderboard/getleaders", methods=["GET"])
+@tag(["leaderboard"])
+# @validate_request(Leaderboard)
+# @validate_response(Leaderboard, 201)
+@validate_response(Error, 400)
+@validate_response(Error, 409)
+async def getleaders():
+    """retrieve the top 10 players from redis based on score"""
+    try: 
+        leaderlist = r.zrange(playerrank,0,9,desc=True,withscores=True)
+        print("leaderlist",leaderlist)
+    except:
+        abort(500,"Internal Server Error Cant update Leaderboard,please try again by refining the input parameters")
+
+    leaderlistStr = "*******LeaderBoard*********\nCurrent Top 10 for Wordle are: \nUsername / Score \n"
+    for i in leaderlist:
+            print("player",i)
+            currentPlayer = ""
+            for j in i:
+                if  isinstance(j,bytes):
+                    j = j.decode("utf-8")
+                currentPlayer+= str(j) + " / "
+            currentPlayer = currentPlayer.rstrip("/ ")
+            currentPlayer+= "\n"
+            leaderlistStr+=currentPlayer
+    return leaderlistStr,200
